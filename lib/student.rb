b56c28945ad213209e331cdb19b73d02cd9ae10f
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-28 01:26:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,9 @@ sql = <<-SQL
 SELECT *
 FROM students
 SQL
-DB[:conn].execute(sql)
-self.new_from_db(row)
+DB[:conn].execute(sql).map do |row|
+  self.new_from_db(row)
+end
   end
 
   def self.find_by_name(name)
