6720a44a8a312e54a63dab71989283c6fb520586
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-28 01:41:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ def self.first_X_students_in_grade_10(x)
 SELECT * FROM students
 WHERE grade < 10
 SQL
-DB[:conn].execute(sql, x).map do |row|
+DB[:conn].execute(sql).map do |row|
 self.new_from_db(row)
 end #end loop
 end
